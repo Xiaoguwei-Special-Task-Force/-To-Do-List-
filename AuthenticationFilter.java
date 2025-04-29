d4c2211a50f643296feece5eea649e593fbdb4f0
xavierxia27 <2146673319@qq.com>
2025-04-29 23:00:10 +0800

    Day6 新增过滤器

---
+++ AuthenticationFilter.java
@@ -0,0 +1,34 @@
+package com.example.AuthenticationFilter;
+
+import java.io.IOException;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebFilter({"/task.html", "/secured/*"})
+public class AuthenticationFilter implements Filter {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpSession session = httpRequest.getSession(false);
+
+        boolean isLoggedIn = (session != null && session.getAttribute("user") != null);
+
+        if (isLoggedIn) {
+            chain.doFilter(request, response);
+        } else {
+            httpResponse.sendRedirect(httpRequest.getContextPath() + "/User.jsp");
+        }
+    }
+
+    // 其他必要方法
+}
