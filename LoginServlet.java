0694e7869342e3490ff34acd5c5761641bd8a2e8
xavierxia27 <2146673319@qq.com>
2025-04-29 18:36:06 +0800

    Day3 后端用户登录注册逻辑

---
+++ LoginServlet.java
@@ -0,0 +1,38 @@
+package com.example.servlet;
+
+import com.example.Dao.UserDAO;
+import com.example.user.User;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        UserDAO userDao = new UserDAO();
+        User user = userDao.loginUser(email, password);
+
+        if (user != null) {
+            // 登录成功，创建session
+            HttpSession session = request.getSession();
+            session.setAttribute("user", user);
+
+            // 重定向到主页或用户仪表板
+            response.sendRedirect("task.html");
+        } else {
+            // 登录失败，返回错误信息
+            request.setAttribute("errorMessage", "邮箱或密码错误");
+            request.getRequestDispatcher("User.jsp").forward(request, response);
+        }
+    }
+}
